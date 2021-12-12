e42a640d01112d8f98ab57859584cc36bc96ef55
Luiz Gustavo <luiz@xpbox.com.br>
2021-12-12 15:30:50 -0300

    Mudanças Menus

--- src/components/editor/menus/Bubble.vue
+++ src/components/editor/menus/Bubble.vue
@@ -1,79 +1,116 @@
 <template>
-    <bubble-menu
-        class="bubble-menu"
-        :tippy-options="{ duration: 100 }"
-        :editor="editor"
-        v-if="editor"
+  <bubble-menu
+    class="bubble-menu"
+    :tippy-options="{ duration: 100 }"
+    :editor="editor"
+    v-if="editor"
+  >
+    <div
+      class="d-flex flex-column px-3 py-3"
+      style="background-color: rgba(0, 0, 0, 0.3); border-radius: 2px"
     >
+      <div class="d-flex justify-content-between mb-2">
         <button
-            @click="editor.chain().focus().toggleBold().run()"
-            :class="{ 'is-active': editor.isActive('bold') }"
+          @click="editor.chain().focus().toggleBold().run()"
+          class="badge text-dark mx-1"
+          :class="{
+            'bg-info': editor.isActive('bold'),
+            'bg-light': !editor.isActive('bold'),
+          }"
         >
-            Bold
+          Negrito
+        </button>
+
+        <button
+          @click="editor.chain().focus().toggleItalic().run()"
+          class="badge text-dark mx-1"
+          :class="{
+            'bg-info': editor.isActive('italic'),
+            'bg-light': !editor.isActive('italic'),
+          }"
+        >
+          Itálico
         </button>
         <button
-            @click="editor.chain().focus().toggleItalic().run()"
-            :class="{ 'is-active': editor.isActive('italic') }"
+          @click="editor.chain().focus().toggleStrike().run()"
+          class="badge text-dark mx-1"
+          :class="{
+            'bg-info': editor.isActive('strike'),
+            'bg-light': !editor.isActive('strike'),
+          }"
         >
-            Italic
+          Tachado
         </button>
         <button
-            @click="editor.chain().focus().toggleStrike().run()"
-            :class="{ 'is-active': editor.isActive('strike') }"
+          @click="editor.chain().focus().toggleHighlight().run()"
+          class="badge text-dark mx-1"
+          :class="{
+            'bg-info': editor.isActive('highlight'),
+            'bg-light': !editor.isActive('highlight'),
+          }"
         >
-            Strike
+          Marca texto
+        </button>
+      </div>
+
+      <div class="d-flex justify-content-between">
+        <button
+          @click="editor.chain().focus().setTextAlign('left').run()"
+          class="badge text-dark mx-1"
+          :class="{
+            'bg-info': editor.isActive('left'),
+            'bg-light': !editor.isActive('left'),
+          }"
+        >
+          Esquerda
         </button>
         <button
-            @click="editor.chain().focus().toggleHighlight().run()"
-            :class="{ 'is-active': editor.isActive('highlight') }"
+          @click="editor.chain().focus().setTextAlign('center').run()"
+          class="badge text-dark mx-1"
+          :class="{
+            'bg-info': editor.isActive('center'),
+            'bg-light': !editor.isActive('center'),
+          }"
         >
-            highlight
+          Centralizar
         </button>
         <button
-            @click="editor.chain().focus().setTextAlign('left').run()"
-            :class="{ 'is-active': editor.isActive({ textAlign: 'left' }) }"
+          @click="editor.chain().focus().setTextAlign('right').run()"
+          class="badge text-dark mx-1"
+          :class="{
+            'bg-info': editor.isActive('right'),
+            'bg-light': !editor.isActive('right'),
+          }"
         >
-            left
+          Direita
         </button>
         <button
-            @click="editor.chain().focus().setTextAlign('center').run()"
-            :class="{
-                'is-active': editor.isActive({ textAlign: 'center' }),
-            }"
+          @click="editor.chain().focus().setTextAlign('justify').run()"
+          class="badge text-dark mx-1"
+          :class="{
+            'bg-info': editor.isActive('justify'),
+            'bg-light': !editor.isActive('justify'),
+          }"
         >
-            center
+          Justificar
         </button>
-        <button
-            @click="editor.chain().focus().setTextAlign('right').run()"
-            :class="{
-                'is-active': editor.isActive({ textAlign: 'right' }),
-            }"
-        >
-            right
-        </button>
-        <button
-            @click="editor.chain().focus().setTextAlign('justify').run()"
-            :class="{
-                'is-active': editor.isActive({ textAlign: 'justify' }),
-            }"
-        >
-            justify
-        </button>
-    </bubble-menu>
+      </div>
+    </div>
+  </bubble-menu>
 </template>
 
 <script>
 import { BubbleMenu } from "@tiptap/vue-3";
 
 export default {
-    props: {
-        editor: {
-            required: true,
-        },
+  props: {
+    editor: {
+      required: true,
     },
-    components: {
-        BubbleMenu,
-    },
+  },
+  components: {
+    BubbleMenu,
+  },
 };
 </script>
 
--- src/components/editor/menus/Floating.vue
+++ src/components/editor/menus/Floating.vue
@@ -1,46 +1,62 @@
 <template>
-    <floating-menu
-        class="floating-menu"
-        :tippy-options="{ duration: 100 }"
-        :editor="editor"
-        v-if="editor"
+  <floating-menu
+    class="floating-menu"
+    :tippy-options="{ duration: 100 }"
+    :editor="editor"
+    v-if="editor"
+  >
+    <div
+      class="d-flex flex-column px-3 py-3"
+      style="background-color: rgba(0, 0, 0, 0.3); border-radius: 2px"
     >
+      <div class="d-flex justify-content-between mb-2">
         <button
-            @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
-            :class="{
-                'is-active': editor.isActive('heading', { level: 1 }),
-            }"
+          @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
+          class="badge text-dark mx-1"
+          :class="{
+            'bg-info': editor.isActive('heading', { level: 1 }),
+            'bg-light': !editor.isActive('heading', { level: 1 }),
+          }"
         >
-            H1
+          Título
+        </button>
+
+        <button
+          @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
+          class="badge text-dark mx-1"
+          :class="{
+            'bg-info': editor.isActive('heading', { level: 2 }),
+            'bg-light': !editor.isActive('heading', { level: 2 }),
+          }"
+        >
+          Subtítulo
         </button>
         <button
-            @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
-            :class="{
-                'is-active': editor.isActive('heading', { level: 2 }),
-            }"
+          @click="editor.chain().focus().toggleBulletList().run()"
+          class="badge text-dark mx-1"
+          :class="{
+            'bg-info': editor.isActive('bulletList'),
+            'bg-light': !editor.isActive('bulletList'),
+          }"
         >
-            H2
+          Lista
         </button>
-        <button
-            @click="editor.chain().focus().toggleBulletList().run()"
-            :class="{ 'is-active': editor.isActive('bulletList') }"
-        >
-            Bullet List
-        </button>
-    </floating-menu>
+      </div>
+    </div>
+  </floating-menu>
 </template>
 
 <script>
 import { FloatingMenu } from "@tiptap/vue-3";
 export default {
-    props: {
-        editor: {
-            required: true,
-        },
+  props: {
+    editor: {
+      required: true,
     },
-    components: {
-        FloatingMenu,
-    },
+  },
+  components: {
+    FloatingMenu,
+  },
 };
 </script>
 
